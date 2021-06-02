1b5efa6bcbf716ccbcc82aac3bf69d2190e5c1b5
NUSRAT2020 <ipaazom@gmail.coom>
2021-06-02 12:44:12 -0400

    class

---
+++ class.js
@@ -0,0 +1,11 @@
+class Student {
+        constructor(firstName , lastName , salary){
+                this.firstName = firstName;
+                this.lastName = lastName ;
+                this.salary = salary;
+        } 
+
+} const student1 = new Student("Nusrat","ipa",500000);
+console.log(student1);
+const student2 = new Student("Musrat","ipa",500000);
+console.log(student2);
